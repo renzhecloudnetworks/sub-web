7933267f67d063e5b78763c8b4d09916757b624e
renzhecloudnetworks <43466927+renzhecloudnetworks@users.noreply.github.com>
2021-02-24 04:18:59 -0600

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -284,6 +284,9 @@ export default {
             ]
           },
 	   {
+            label: "ACL4SSR",
+            options: [
+              {
                 label: "ACL4SSR_Online 默认版 分组比较全(与Github同步)",
                 value:
                   "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini"
